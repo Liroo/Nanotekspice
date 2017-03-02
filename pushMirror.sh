8d43bd7008a73509c294881fcac5eef650157870
Liroo <pierre-monge@hotmail.fr>
2017-03-02 05:36:18 +0100

    make the script more readable

--- pushMirror.sh
+++ pushMirror.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
 # push to epitech repository
+echo
 git push origin master
 if [ $? -eq 0 ]
 then
+  echo
   echo '√ Pushed to epitech repository'
-elif
+else
+  echo
   echo '✗ Failed to push to epitech repository'
 fi
 
 # push to mirror repository (https://github.com/Liroo/Nanotekspice)
+echo
 git push --mirror git@github.com:Liroo/Nanotekspice.gi
 if [ $? -eq 0 ]
 then
+  echo
   echo '√ Pushed to mirror repository'
-elif
+else
+  echo
   echo '✗ Failed to push to mirror repository'
 fi
